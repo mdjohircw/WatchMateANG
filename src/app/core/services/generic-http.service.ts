50a14d4a5702f63b0852e734b282945bc3a20700
Md Johirull Islam <143317864+mdjohircw@users.noreply.github.com>
2025-08-19 02:37:08 +0600

    User dashboard customize

--- src/app/core/services/generic-http.service.ts
+++ src/app/core/services/generic-http.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class GenericHttpService<T> {
   constructor(private http: HttpClient) {}
   private RootUrl='https://localhost:7253';
-  //private RootUrl='https://api.watchandearn.org'; 
+ // private RootUrl='https://api.watchandearn.org'; 
  // private RootUrl='https://upstartloan-api.codehosting.xyz';
   getAll<T>(apiUrl: string): Observable<T[]> {
     return this.http.get<T[]>(`${this.RootUrl}/${apiUrl}`);  // No need to manually set headers
